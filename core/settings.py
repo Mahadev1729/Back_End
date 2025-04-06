49066245c1e2ec2e1790b17309215eef2871ab38
Mahadev1729 <devcampus17@gmail.com>
2025-04-06 23:37:34 +0530

    added external app

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+   
 ]
+
+EXTERNAL_APPS=[
+    'home'
+]
+INSTALLED_APPS=INSTALLED_APPS+EXTERNAL_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
