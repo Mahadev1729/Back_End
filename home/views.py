fa133947872ead442f4c151d320933df4d1b830b
Mahadev1729 <devcampus17@gmail.com>
2025-04-08 00:44:30 +0530

    to see data in ui

--- home/views.py
+++ home/views.py
@@ -4,7 +4,16 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request,"index.html")
+    #data from the database
+    peoples=[
+        {"name":"Mahadev","age":34},
+        {"name":"Akash","age":44},
+        {"name":"vinod","age":38},
+        {"name":"Sandeep","age":45},
+    ]
+    #Context
+    
+    return render(request,"index.html",context={'peoples':peoples})
 
 def admin_panel(request):
     return HttpResponse("""<h1>This is admin pannel</h1>""")
