e3980570ce628bbaa3025c13fa9cc2d83c6d901f
Mahadev1729 <devcampus17@gmail.com>
2025-04-07 23:59:58 +0530

    try to run server

--- home/views.py
+++ home/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("Hey! I'm Mahadev")
+
+def admin_panel(request):
+    return HttpResponse("This is admin pannel")
+
