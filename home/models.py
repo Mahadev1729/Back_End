8c03bc9a236b346063f4ec3f0b3375893ae1442b
Mahadev1729 <devcampus17@gmail.com>
2025-04-09 16:14:16 +0530

    know about database

--- home/models.py
+++ home/models.py
@@ -7,7 +7,7 @@ class Students(models.Model):
     name=models.CharField(max_length=100)
     age=models.IntegerField()
     email=models.EmailField()
-    address=models.TextField();
+    address=models.TextField(null=True,blank=True);
     image=models.ImageField()
     files=models.FileField()
     
