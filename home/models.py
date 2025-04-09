82072afc45cececad099ca27899cb26e7c367a35
Mahadev1729 <devcampus17@gmail.com>
2025-04-09 16:00:59 +0530

    solve error using quick ifx

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+from django.db import models # type: ignore
 
 # Create your models here.
+
+class Student(models.Model):
+    name=models.CharField(max_length=100)
+    age=models.IntegerField()
+    email=models.EmailField()
+    address=models.TextField();
+    image=models.ImageField()
+    files=models.FileField()
+    
+    
